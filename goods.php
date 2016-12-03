15c63774bcaad99a8f551b6dc5ed455efeb104db
Kevinl7 <kevinl7@163.com>
2016-12-03 11:43:44 +0800

    上传

---
+++ goods.php
@@ -0,0 +1,16 @@
+<?php
+
+$goods = [
+    ['title'=>'好商品1','price'=>11.3 , 'img'=>'img/goods-1.jpg'],
+    ['title'=>'好商品2','price'=>12.4 , 'img'=>'img/goods-2.jpg'],
+    ['title'=>'好商品3','price'=>37.5 , 'img'=>'img/goods-3.jpg'],
+    ['title'=>'好商品4','price'=>86.9 , 'img'=>'img/goods-4.jpg'],
+    ['title'=>'好商品5','price'=>86.9 , 'img'=>'img/goods-5.jpg'],
+    ['title'=>'好商品6','price'=>86.9 , 'img'=>'img/goods-6.jpg'],
+];
+
+echo json_encode($goods);
+
+
+
+?>
